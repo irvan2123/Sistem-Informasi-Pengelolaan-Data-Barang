ae564ed64cf263b42411ea243c7ad31105c74434
Irvan Nur Apendi <irvannurapendi@gmail.com>
2017-03-21 00:05:56 +0700

    Progress tubes
    Update barang & Penyedia

--- Barang.java
+++ Barang.java
@@ -9,5 +9,38 @@
  * @author IRFAN
  */
 public class Barang {
-   
+
+    private String nama;
+    private double harga;
+    private int jumlahBarang;
+
+    public Barang(String nama, double harga, int jumlahBarang) {
+        this.nama = nama;
+        this.jumlahBarang = jumlahBarang;
+        this.harga = harga;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
+        this.jumlahBarang = jumlahBarang;
+    }
 }
--- Penyedia.java
+++ Penyedia.java
@@ -9,5 +9,56 @@
  * @author IRFAN
  */
 public class Penyedia {
+    private String nama;
+    private Barang[] daftarBarang = new Barang[50]; //asumsi barang yang tersedia ada 50
+    private String alamat;
+    private int jumlahBarang;
+    
+    public Penyedia (String nama, String alamat , int maxJumBarang , int jumlahBarang) {
+        this.alamat = alamat;
+        this.nama = nama;
+        this.daftarBarang = new Barang[maxJumBarang];
+        this.jumlahBarang = jumlahBarang;
+    }
+    
+    public void tambahBarang (Barang b) {
+        while (this.getJumlahBarang() < this.getDaftarBarang().length) {
+            getDaftarBarang()[getJumlahBarang()] = b;
+            this.setJumlahBarang(this.getJumlahBarang() + 1);
+        }
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Barang[] getDaftarBarang() {
+        return daftarBarang;
+    }
+
+    public void setDaftarBarang(Barang[] daftarBarang) {
+        this.daftarBarang = daftarBarang;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
+        this.jumlahBarang = jumlahBarang;
+    }
+    
     
 }
