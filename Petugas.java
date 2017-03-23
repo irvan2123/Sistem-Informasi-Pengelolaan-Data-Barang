9049dff7635c796081195acc7c7dd86469563fb9
DanielChristopher <danielsirait@Daniels-MacBook-Pro.local>
2017-03-23 18:24:37 +0700

    Petugas - Progress Tubes #1
    - Membuat class
    - Menambahkan attribute

--- Petugas.java
+++ Petugas.java
@@ -9,5 +9,51 @@
  * @author IRFAN
  */
 public class Petugas {
+    private String nama; 
+    private String IDPetugas;
+    private int umur;
+    private String alamat;
+    
+    public Petugas(String nama, String IDPetugas, int umur, String alamat) {
+        this.nama = nama;
+        this.IDPetugas = IDPetugas;
+        this.umur = umur;
+        this.alamat = alamat;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getIDPetugas() {
+        return IDPetugas;
+    }
+
+    public void setIDPetugas(String IDPetugas) {
+        this.IDPetugas = IDPetugas;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    
+    
     
 }
